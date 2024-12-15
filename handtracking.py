0ee99ffac38655b786b8f522007c4d209e1914a3
Shun Gram <mike53669@gmail.com>
2024-12-15 12:00:02 -0800

    refactor hand recognition code into a module

--- handtracking.py
+++ handtracking.py
@@ -2,40 +2,47 @@ import cv2
 import mediapipe as mp
 import time # to check the frame rate
 
-cap = cv2.VideoCapture(0)
-# Open camera with index, open video if given file name
+class HandDetector:
+    def __init__(self):
+        self.mpHands = mp.solutions.hands
+        # TODO(shunxian): repeatedly call method in a hand, remember the context?
+        self.hands = self.mpHands.Hands()
+        self.mpDraw = mp.solutions.drawing_utils
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(imgRGB)
+        if results.multi_hand_landmarks:
+            for each_hand in results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, each_hand, self.mpHands.HAND_CONNECTIONS)
+        # img is changed in place
 
-mpDraw = mp.solutions.drawing_utils
 
-pTime = 0
-cTime = 0
+# driver code
+def main():
+    cap = cv2.VideoCapture(0)
+    hand_detector = HandDetector()
+    # Open camera with index, open video if given file name
+    
+    pTime = 0
+    cTime = 0
+    while True:
+        # camera input
+        success, img = cap.read()
 
-while True:
-    # camera input
-    success, img = cap.read()
+        hand_detector.findHands(img)
+        
+        # frame rate calculation
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
 
-    # take a picture
-    # TODO(shunxian): how often are we calling it
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
-    if results.multi_hand_landmarks: # hand landmarks are detected, next step is to connect the dots
-        for each_hand in results.multi_hand_landmarks: # for each hand, draw the landmarks separately
-            for i, land_mark in enumerate(each_hand.landmark):
-                # TODO(shunxian): x,y,z coordinates what is the z coordinate for?
-                pass
-            mpDraw.draw_landmarks(img, each_hand, mpHands.HAND_CONNECTIONS)
+        # program output
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
 
-    # frame rate calculation
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-
-    # program output
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+if __name__ == "__main__":
+    main()
